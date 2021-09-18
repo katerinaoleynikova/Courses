02547562771ef4af36e74faeeba9e8f7a6d3ddea
katerinaoleynikova <77380715+katerinaoleynikova@users.noreply.github.com>
2021-09-18 18:21:11 +0300

    Update R_Programming_HW_1.R

--- R_Programming_HW_1.R
+++ R_Programming_HW_1.R
@@ -1,7 +1,7 @@
 # Homework 1
 # Deadline 15 September at 23:59
 
-# 1. Read the data (hw1.txt) that contain the three colum. w gene expression level (each gene - each column) at dif.condit.
+# 1. Read the data (hw1_r_programming.txt) that contain the three colum. w gene expression level (each gene - each column) at dif.condit.
 data <- read.table("hw1_r_programming.txt")
 data
 
